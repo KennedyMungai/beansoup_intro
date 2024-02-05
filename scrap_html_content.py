22ec88ddceb24e89af6ced31795f34822f01cec9
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-02-05 12:08:25 +0300

    Made a simple update to remove an unwanted indent in the file documentation

--- scrap_html_content.py
+++ scrap_html_content.py
@@ -1,4 +1,4 @@
-    """A simple script mean to scrap the data off of a static web page"""
+"""A simple script mean to scrap the data off of a static web page"""
 import requests
 
 URL = 'http://127.0.0.1:5500/web_content/index.html'
