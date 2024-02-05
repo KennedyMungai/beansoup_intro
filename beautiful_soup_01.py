10e5abb05d73b84acf301fd0b38779d8bb50ee7a
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-02-05 12:22:49 +0300

    Using beautiful soup to get html info from the web page

--- beautiful_soup_01.py
+++ beautiful_soup_01.py
@@ -1,1 +1,12 @@
 """A simple use case for beautiful soup"""
+import requests
+from bs4 import BeautifulSoup
+
+
+URL = 'http://127.0.0.1:5500/web_content/index.html'
+
+page = requests.get(URL, timeout=300)
+
+soup = BeautifulSoup(page.content, 'html.parser')
+
+print(soup.prettify())
