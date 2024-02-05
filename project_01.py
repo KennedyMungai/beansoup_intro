35af9890139644846a337ed465eb2ad156612c05
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-02-05 14:20:45 +0300

    Basic project setup

--- project_01.py
+++ project_01.py
@@ -1,1 +1,9 @@
 """Some Project"""
+import requests
+from bs4 import BeautifulSoup
+
+URL = 'http://127.0.0.1:5500/web_content/index.html'
+
+page = requests.get(URL, timeout=300)
+
+soup = BeautifulSoup(page.content, 'html.parser')
